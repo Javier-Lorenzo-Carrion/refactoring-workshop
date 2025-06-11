bfa76fd08d9d9e120ea098bfe6c23e12fa489e7d
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-06-11 11:10:20 +0100

    feat: remove responsability between invoice and invoice service

--- src/core/moveResponsibility/Invoice.ts
+++ src/core/moveResponsibility/Invoice.ts
@@ -1,5 +1,13 @@
 export class Invoice {
-  grossAmount: number;
-  taxPercentage: number;
-  numberOfItems: number;
+	grossAmount: number;
+	taxPercentage: number;
+	numberOfItems: number;
+
+	calculateNetAmount(amount: string, tax: string): string {
+    const invoice = new Invoice();
+		invoice.grossAmount = Number(amount);
+		invoice.taxPercentage = Number(tax);
+		const taxAmount = (invoice.taxPercentage / 100) * invoice.grossAmount;
+		return (invoice.grossAmount - taxAmount).toString();
+	}
 }
--- src/tests/moveResponsibility/invoice.test.ts
+++ src/tests/moveResponsibility/invoice.test.ts
@@ -1,9 +1,9 @@
-import { InvoiceService } from "../../core/moveResponsibility/InvoiceService";
 
-describe('The Invoice Service', ()=>{
-  it('calculates net amount', ()=>{
-    const invoiceService = new InvoiceService();
-    const result = invoiceService.calculateNetAmount("100", "10");
-    expect(result).toBe("90");
-  })
-})
+import { Invoice } from '../../core/moveResponsibility/Invoice';
+
+describe('The Invoice Service', () => {
+	it('calculates net amount', () => {
+		const result = new Invoice().calculateNetAmount('100', '10');
+		expect(result).toBe('90');
+	});
+});
